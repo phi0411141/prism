564cb9eff0649190deeff97edd97bebef53618e8
phi0411141 <hongphipham97@gmail.com>
2017-10-18 11:22:30 +0700

    ahihi

--- components/prism-javascript.js
+++ components/prism-javascript.js
@@ -1,9 +1,14 @@
 Prism.languages.javascript = Prism.languages.extend('clike', {
-	'keyword': /\b(as|async|await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/,
+	'keyword': /\b(as|async|await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield|byte|assert|final|strictfp|synchronized|throws|undefined|extern|internal|sizeof|virtual|using|volatile|namespace|implicit|readonly|delegate|out|struct|unsigned|sign|float|int|long|sbyte|base|decimal|event|string|override|double|constexpr|const_cast|friend|wchar_t|thread_local|static_assert|static_cast|union|alignas|alignof|auto|xor|cfunction|enddeclare|endfor|endforeach|endif|sealed|endswitch|endwhile|include_once|global|require_once|echo|parent|annotation|reified|companion|crossinline|)\b/,
 	'number': /\b-?(0[xX][\dA-Fa-f]+|0[bB][01]+|0[oO][0-7]+|\d*\.?\d+([Ee][+-]?\d+)?|NaN|Infinity)\b/,
 	// Allow for all non-ASCII characters (See http://stackoverflow.com/a/2008444)
 	'function': /[_$a-zA-Z\xA0-\uFFFF][_$a-zA-Z0-9\xA0-\uFFFF]*(?=\()/i,
-	'operator': /-[-=]?|\+[+=]?|!=?=?|<<?=?|>>?>?=?|=(?:==?|>)?|&[&=]?|\|[|=]?|\*\*?=?|\/=?|~|\^=?|%=?|\?|\.{3}/
+	'operator': /-[-=]?|\+[+=]?|!=?=?|<<?=?|>>?>?=?|=(?:==?|>)?|&[&=]?|\|[|=]?|\*\*?=?|\/=?|~|\^=?|%=?|\?|\.{3}/,
+	'selector': /[^\{\}\s][^\{\};]*?(?=\s*\{)/,
+	'important': /\B!important\b/i,
+	'doctype': /<!DOCTYPE[\s\S]+?>/i,
+	'cdata': /<!\[CDATA\[[\s\S]*?]]>/i,
+	'url': /url\((?:(["'])(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1|.*?)\)/i,
 });
 
 Prism.languages.insertBefore('javascript', 'keyword', {
